a0b0a136d60b9bddf6a494fcb6896413029ae41e
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-02-27 16:16:50 +0200

    Virtual machines can now be browsed from the inventory

--- src/pvc/inventory.py
+++ src/pvc/inventory.py
@@ -1,7 +1,10 @@
 """
-Docstring should be here
+Docstring should go here
 
 """
+
+import pyVmomi
+import pvc.virtualmachine
 
 from pvc.menu import Menu, MenuItem
 
@@ -29,7 +32,8 @@ class Inventory(object):
             ),
             MenuItem(
                 tag='VMs and Templates',
-                description='Manage VMs and templates'
+                description='Manage VMs and templates',
+                on_select=self.virtual_machine_menu
             ),
             MenuItem(
                 tag='Datastores',
@@ -42,7 +46,43 @@ class Inventory(object):
         ]
 
         menu = Menu(
-            text='Inventory Menu',
+            title='Inventory Menu',
+            text='Select an item from the inventory',
+            items=items,
+            dialog=self.dialog,
+            width=70,
+        )
+        menu.display()
+
+    def virtual_machine_menu(self):
+        self.dialog.infobox(
+            text='Retrieving Virtual Machines ...',
+            width=40
+        )
+
+        view = self.agent.get_vm_view()
+
+        properties = self.agent.collect_properties(
+            view_ref=view,
+            obj_type=pyVmomi.vim.VirtualMachine,
+            path_set=['name', 'runtime.powerState'],
+            include_mors=True
+        )
+
+        view.DestroyView()
+
+        items = [
+            MenuItem(
+                tag=vm['name'],
+                description=vm['runtime.powerState'],
+                on_select=pvc.virtualmachine.VirtualMachine,
+                on_select_args=(vm['obj'], self.dialog)
+            ) for vm in properties
+        ]
+
+        menu = Menu(
+            title='Virtual Machines',
+            text='Select a Virtual Machine from the menu that you wish to manage',
             items=items,
             dialog=self.dialog
         )

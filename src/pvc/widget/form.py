91e10048ff8c6081dd974f98372a78b624bbc33a
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-03-04 16:22:10 +0200

    Form and FormElement can be used to create mixed forms as well

--- src/pvc/widget/form.py
+++ src/pvc/widget/form.py
@@ -9,9 +9,17 @@ __all__ = ['Form', 'FormElement']
 
 
 class FormElement(object):
-    def __init__(self, label, item, xi=20, field_length=60, input_length=0):
+    def __init__(self, label, item, xi=20, field_length=60, input_length=0, attributes=0x0):
         """
-        docstring
+        A form element
+
+        Args:
+           label       (str): Label of the element
+           item        (str): Initial value for the element
+           xi          (int): Display item value in column 'xi'
+           field_lenght(int): Number of characters used to display the item
+           field_input (int): Input field characters
+           attributes  (int): A bitmask used for elements used in mixed forms
 
         """
         self.label = label
@@ -19,9 +27,10 @@ class FormElement(object):
         self.xi = xi
         self.field_length = field_length
         self.input_length = input_length
+        self.attributes = attributes
 
 class Form(object):
-    def __init__(self, dialog, form_elements, title='', text='', height=0, width=0, form_height=0):
+    def __init__(self, dialog, form_elements, title='', text='', height=0, width=0, form_height=0, mixed_form=False):
         """
         A form widget
 
@@ -37,20 +46,27 @@ class Form(object):
         """
         self.dialog = dialog
         self.form_elements = form_elements
-        self._labels = [e.label for e in self.form_elements]
-        self._elements = [(element.label, row + 1, 1, element.item, row + 1, element.xi, element.field_length, element.input_length) for row, element in enumerate(self.form_elements)]
         self.title = title
         self.text = text
         self.height = height
         self.width = width
         self.form_height = form_height
+        self.mixed_form = mixed_form
+        self._labels = [e.label for e in self.form_elements]
+
+        if self.mixed_form:
+            self.form = self.dialog.mixedform
+            self._elements = [(e.label, row + 1, 1, e.item, row + 1, e.xi, e.field_length, e.input_length, e.attributes) for row, e in enumerate(self.form_elements)]
+        else:
+            self.form = self.dialog.form
+            self._elements = [(e.label, row + 1, 1, e.item, row + 1, e.xi, e.field_length, e.input_length) for row, e in enumerate(self.form_elements)]
 
     def display(self):
         """
         docstring
 
         """
-        code, items = self.dialog.form(
+        code, items = self.form(
             title=self.title,
             text=self.text,
             elements=self._elements,

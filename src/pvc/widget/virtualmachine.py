9ff56914318ebf0a02866daf661737a16cfae0c3
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-03-04 17:50:52 +0200

    PVC can now display resource usage information for a Virtual Machine

--- src/pvc/widget/virtualmachine.py
+++ src/pvc/widget/virtualmachine.py
@@ -35,6 +35,11 @@ class VirtualMachineWidget(object):
                 on_select=self.general_info
             ),
             MenuItem(
+                tag='Resources',
+                description='Resources usage information ',
+                on_select=self.resources_info
+            ),
+            MenuItem(
                 tag='Configuration',
                 description='Virtual Machine settings'
             ),
@@ -78,16 +83,46 @@ class VirtualMachineWidget(object):
         properties = data.pop()
 
         elements = [
-            FormElement(label='Guest OS', item=properties.get('config.guestFullName', 'Unknown')),
-            FormElement(label='VM Version', item=properties.get('config.version')),
-            FormElement(label='CPU', item='{} vCPU(s)'.format(properties.get('config.hardware.numCPU'))),
-            FormElement(label='Memory', item='{} MB'.format(properties.get('config.hardware.memoryMB'))),
-            FormElement(label='Memory Overhead', item='{} MB'.format(properties.get('summary.quickStats.consumedOverheadMemory'))),
-            FormElement(label='VMware Tools', item='{} ({})'.format(properties.get('guest.toolsRunningStatus'), properties.get('guest.toolsVersionStatus'))),
-            FormElement(label='IP Address', item=properties.get('guest.ipAddress', 'Unknown')),
-            FormElement(label='DNS Name', item=properties.get('guest.hostName', 'Unknown')),
-            FormElement(label='State', item=properties.get('runtime.powerState')),
-            FormElement(label='Host', item=self.obj.runtime.host.name),
+            FormElement(
+                label='Guest OS',
+                item=properties.get('config.guestFullName', 'Unknown')
+            ),
+            FormElement(
+                label='VM Version',
+                item=properties.get('config.version')
+            ),
+            FormElement(
+                label='CPU',
+                item='{} vCPU(s)'.format(properties.get('config.hardware.numCPU'))
+            ),
+            FormElement(
+                label='Memory',
+                item='{} MB'.format(properties.get('config.hardware.memoryMB'))
+            ),
+            FormElement(
+                label='Memory Overhead',
+                item='{} MB'.format(properties.get('summary.quickStats.consumedOverheadMemory'))
+            ),
+            FormElement(
+                label='VMware Tools',
+                item='{} ({})'.format(properties.get('guest.toolsRunningStatus'), properties.get('guest.toolsVersionStatus'))
+            ),
+            FormElement(
+                label='IP Address',
+                item=properties.get('guest.ipAddress', 'Unknown')
+            ),
+            FormElement(
+                label='DNS Name',
+                item=properties.get('guest.hostName', 'Unknown')
+            ),
+            FormElement(
+                label='State',
+                item=properties.get('runtime.powerState')
+            ),
+            FormElement(
+                label='Host',
+                item=self.obj.runtime.host.name
+            ),
         ]
 
         form = Form(
@@ -95,6 +130,73 @@ class VirtualMachineWidget(object):
             form_elements=elements,
             title=self.obj.name,
             text='\nVirtual Machine General Information\n'
+        )
+
+        return form.display()
+
+    def resources_info(self):
+        """
+        Resources usage information
+
+        """
+        self.dialog.infobox(
+            title=self.obj.name,
+            text='Retrieving resources usage information ...'
+        )
+
+        view = self.agent.get_list_view([self.obj])
+        data = self.agent.collect_properties(
+            view_ref=view,
+            obj_type=pyVmomi.vim.VirtualMachine,
+            path_set=[
+                'summary.quickStats.overallCpuUsage',
+                'summary.quickStats.hostMemoryUsage',
+                'summary.quickStats.guestMemoryUsage',
+                'summary.storage.committed',
+                'summary.storage.uncommitted',
+                'summary.storage.unshared',
+            ]
+        )
+        view.DestroyView()
+        properties = data.pop()
+
+        # Convert storage information into GB first
+        properties['summary.storage.committed'] /= 1073741824
+        properties['summary.storage.unshared'] /= 1073741824
+        properties['summary.storage.uncommitted'] /= 1073741824
+
+        elements = [
+            FormElement(
+                label='Consumed Host CPU',
+                item='{} MHz'.format(properties.get('summary.quickStats.overallCpuUsage'))
+            ),
+            FormElement(
+                label='Consumed Host Memory',
+                item='{} MB'.format(properties.get('summary.quickStats.hostMemoryUsage'))
+            ),
+            FormElement(
+                label='Active Guest Memory',
+                item='{} MB'.format(properties.get('summary.quickStats.guestMemoryUsage'))
+            ),
+            FormElement(
+                label='Provisioned Storage',
+                item='{} GB'.format(round(properties.get('summary.storage.committed') + properties.get('summary.storage.uncommitted'), 2))
+            ),
+            FormElement(
+                label='Non-shared Storage',
+                item='{} GB'.format(round(properties.get('summary.storage.unshared'), 2))
+            ),
+            FormElement(
+                label='Used Storage',
+                item='{} GB'.format(round(properties.get('summary.storage.committed'), 2))
+            ),
+        ]
+
+        form = Form(
+            dialog=self.dialog,
+            form_elements=elements,
+            title=self.obj.name,
+            text='\nVirtual Machine Resources Usage Information\n'
         )
 
         return form.display()

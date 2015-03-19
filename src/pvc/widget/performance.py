7e0b51dad9f697d118da42f78c754e3343ec6f0c
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-03-19 10:11:12 +0200

    Initial commit of pvc.widget.performance module

---
+++ src/pvc/widget/performance.py
@@ -0,0 +1,100 @@
+"""
+Performance Metric Widgets
+
+"""
+
+import pyVmomi
+import pvc.widget.menu
+import pvc.widget.form
+
+__all__ = ['PerformanceWidget']
+
+
+class PerformanceWidget(object):
+    def __init__(self, agent, dialog, obj):
+        """
+        Performance Widget
+
+        Args:
+            agent                 (VConnector): A VConnector instance
+            dialog             (dialog.Dialog): A Dialog instance
+            obj    (pyVmomi.vim.ManagedEntity): A managed entity
+
+        """
+        if not isinstance(obj, pyVmomi.vim.ManagedEntity):
+            raise TypeError('Need a vim.ManagedEntity instance')
+
+        self.agent = agent
+        self.dialog = dialog
+        self.obj = obj
+        self.pm = self.agent.si.content.perfManager
+        self.display()
+
+    def display(self):
+        items = [
+            pvc.widget.menu.MenuItem(
+                tag='Summary',
+                description='Performance provider summary',
+                on_select=self.provider_summary
+            ),
+            pvc.widget.menu.MenuItem(
+                tag='Metrics',
+                description='Available performance metrics',
+                on_select=self.metrics
+            ),
+            pvc.widget.menu.MenuItem(
+                tag='Real-time',
+                description='Real-time performance metrics'
+            ),
+            pvc.widget.menu.MenuItem(
+                tag='Historical',
+                description='Historical performance metrics'
+            ),
+        ]
+
+        menu = pvc.widget.menu.Menu(
+            title=self.obj.name,
+            text='Performance Metrics',
+            items=items,
+            dialog=self.dialog
+        )
+        menu.display()
+
+    def summary(self):
+        """
+        Performance provider summary information
+
+        """
+        self.dialog.infobox(
+            title=self.obj.name,
+            text='Retrieving information ...'
+        )
+
+        provider_summary = self.pm.QueryPerfProviderSummary(
+            entity=self.obj
+        )
+
+        elements = [
+            pvc.widget.form.FormElement(
+                label='Real-time statistics support',
+                item=str(provider_summary.currentSupported)
+            ),
+            pvc.widget.form.FormElement(
+                label='Historical statistics support',
+                item=str(provider_summary.summarySupported)
+            ),
+            pvc.widget.form.FormElement(
+                label='Refresh rate (seconds)',
+                item=str(provider_summary.refreshRate)
+            )
+        ]
+
+        form = pvc.widget.form.Form(
+            title=self.obj.name,
+            text='Performance provider summary information',
+            form_elements=elements
+            dialog=self.dialog,
+        )
+
+        form.display()
+

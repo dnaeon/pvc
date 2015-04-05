932963774ee833b5e65205093412d73bf43ab0d3
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-04-05 16:36:00 +0300

    Initial commit of pvc.widget.datacenter module

---
+++ src/pvc/widget/datacenter.py
@@ -0,0 +1,71 @@
+"""
+Datacenter Widgets
+
+"""
+
+import pyVmomi
+
+import pvc.widget.menu
+
+__all__ = ['DatacenterWidget']
+
+
+class DatacenterWidget(object):
+    def __init__(self, agent, dialog, obj):
+        """
+        Datacenter Widget
+
+        Args:
+            agent      (VConnector): A VConnector instance
+            dialog  (dialog.Dialog): A Dialog instance
+            obj    (vim.Datacenter): A vim.Datacenter managed entity
+
+        """
+        self.agent = agent
+        self.dialog = dialog
+        self.obj = obj
+        self.display()
+
+    def display(self):
+        items = [
+            pvc.widget.menu.MenuItem(
+                tag='Summary',
+                description='General information',
+                on_select=self.summary
+            ),
+        ]
+
+        menu = pvc.widget.menu.Menu(
+            items=items,
+            dialog=self.dialog,
+            title=self.obj.name,
+            text='Select item from menu'
+        )
+
+        menu.display()
+
+    def summary(self):
+        """
+        Datacenter general information
+
+        """
+        self.dialog.infobox(
+            title=self.obj.name,
+            text='Retrieving information ...'
+        )
+
+        elements = [
+            pvc.widget.form.FormElement(
+                label='Overall Status',
+                item=self.obj.overallStatus
+            ),
+        ]
+
+        form = pvc.widget.form.Form(
+            dialog=self.dialog,
+            form_elements=elements,
+            title=self.obj.name,
+            text='Datacenter general information'
+        )
+
+        form.display()

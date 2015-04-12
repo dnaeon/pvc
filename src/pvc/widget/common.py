0f957027711a13deaa7a2c7e8dac54256804f163
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-04-12 13:21:14 +0300

    Add missing names to __all__ in pvc.widget.common

--- src/pvc/widget/common.py
+++ src/pvc/widget/common.py
@@ -21,9 +21,9 @@ __all__ = [
     'rename', 'network_menu', 'datastore_menu',
     'host_menu', 'hostmount_menu', 'virtual_machine_menu',
     'session_menu', 'alarm_menu', 'choose_folder',
-    'choose_datacenter', 'inventory_search_by_dns',
-    'inventory_search_by_ip', 'inventory_search_by_uuid',
-    'datacenter_menu', 'remove',
+    'choose_datacenter', 'choose_cluster', 'choose_datastore',
+    'inventory_search_by_dns', 'inventory_search_by_ip',
+    'inventory_search_by_uuid', 'datacenter_menu', 'remove',
 ]
 
 

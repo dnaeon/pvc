9eb7b8f67f97d134f8073dbc048b888b75d032c9
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-04-14 12:46:45 +0300

    Issue #2: Add a 'debug' menu item to DatacenterWidget

--- src/pvc/widget/datacenter.py
+++ src/pvc/widget/datacenter.py
@@ -32,6 +32,7 @@ import pyVmomi
 import pvc.widget.alarm
 import pvc.widget.common
 import pvc.widget.cluster
+import pvc.widget.debug
 import pvc.widget.event
 import pvc.widget.hostsystem
 import pvc.widget.menu
@@ -127,6 +128,12 @@ class DatacenterWidget(object):
                 description='View triggered alarms',
                 on_select=pvc.widget.common.alarm_menu,
                 on_select_args=(self.agent, self.dialog, self.obj)
+            ),
+            pvc.widget.menu.MenuItem(
+                tag='Debug',
+                description='Start a Python REPL console',
+                on_select=pvc.widget.debug.DebugWidget,
+                on_select_args=(locals(), globals())
             ),
         ]
 

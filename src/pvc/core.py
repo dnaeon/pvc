f173548cd8c35d00f51c7a21d1d6525bdb259748
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-05-08 11:13:05 +0300

    PEP-476: Disable SSL certificate verification by default
    * Most vSphere (ESXi, vCenter) installations come with
      self-signed certificates, so we need to monkey patch
      the `ssl` module in order to be able to connect to such hosts,
      otherwise on recent Python versions we get an exception raised

--- src/pvc/core.py
+++ src/pvc/core.py
@@ -27,6 +27,17 @@ Core Widgets
 
 """
 
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
 import pyVmomi
 
 import requests
